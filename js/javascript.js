b69f30c1f78b87b4cfd0f67eb9c09c327ef06b6f
Bryan Samek <bsamek@hawk.iit.edu>
2023-05-02 22:56:57 -0500

    attempting to make map appear

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,34 @@
-// Initialize and add the map
-let canvas;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("canvas"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    position: position,
-    title: "Uluru",
-  });
+function init() {
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(52.23029990325073, 21.000073548420396); {
+	var mapO = {
+		center: myLocation,
+		zoom: 10;
+		zoomControl: true;
+		scaleControl: true;
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	});
+  }
 }
+	var myMap= new google.maps.Map(el,mapO);
+	
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		title: "Capital of Poland",
+		animation: google.maps.Animation.DROP,
+		icon: 
+	});
+	marker.setMap(myMap);
+	var contentPL= '<h1>Warsaw, Poland</h1> <p> My family is from Poland and Warsaw is the capital of Poland with many beautiful areas to visit. Although my family is not from Warsaw the city has many interesting sites and a very rich history.</p>';
+}
+    var infowindow = new google.maps.InfoWindow({
+		content: contentPL
+	});
+	google.maps.event.addListener(marker, 'mouseover'function() {
+		infowindow.open(myMap, marker);
 
-initMap();
+google.maps.event.addDomListener(window,'load', init);
