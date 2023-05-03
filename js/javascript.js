92fecc9152549d8e2fc7530c48feb68d37fda96e
Bryan Samek <bsamek@hawk.iit.edu>
2023-05-02 22:39:26 -0500

    trying to make map appear

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,27 @@
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('canvas'), {
-	  center: {lat: 52.23029990325073, lng: 21.000073548420396}
-	  zoom: 10;
-	});
-	var plCapital = new google.maps.Marker({
-		position: {lat: 52.2, lng: 21}
-		map; map,
-		title: 'Capital of Poland'
-	});
-	var plCapitalInfo =  new google.maps.InfoWindow({
-		content:<h1>Warsaw, Poland</h1> <p> My family is from Poland and Warsaw is the capital of Poland with many beautiful areas to visit. Although my family is not from Warsaw the city has many interesting sites and a very rich history.</p>';
-	});
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("canvas"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+
+initMap();
