88fc0f4eb9c57d82254a5563b1a5f6fd25fcb9df
Bryan Samek <bsamek@hawk.iit.edu>
2023-05-02 22:21:23 -0500

    Adding API again + syntax fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-function init() {
+function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 52.23029990325073, lng: 21.000073548420396}
 	  zoom: 10;
@@ -11,4 +11,4 @@ function init() {
 	var plCapitalInfo =  new google.maps.InfoWindow({
 		content:<h1>Warsaw, Poland</h1> <p> My family is from Poland and Warsaw is the capital of Poland with many beautiful areas to visit. Although my family is not from Warsaw the city has many interesting sites and a very rich history.</p>';
 	});
-	
+}
